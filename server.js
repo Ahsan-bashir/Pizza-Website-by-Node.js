3864f4f4f5b3dcc09596cb0e54b8bf1f95c786a7
Ahsan-bashir <bteeccc@gmail.com>
2023-08-20 23:21:24 +0500

    Created our own http Server for pizza website

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+http.createServer((request, response) => {
+    if (request.url === '/') {
+        console.log(request.url);
+        response.writeHead(200, { 'content-type': "text/html" })
+        response.write("<h1> Pizza </h1>")
+        response.end()
+    }else if(request.url === '/menu'){
+        console.log(request.url);
+        response.writeHead(200, { 'content-type': "text/html" })
+        response.write("<h1> Pepperoni </h1>")
+        response.end()
+    }
+}).listen(3000)
