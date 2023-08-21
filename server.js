e08b462526073c6dcea2e83604414c8e2019fc5d
Ahsan-bashir <bteeccc@gmail.com>
2023-08-21 09:51:10 +0500

    Build endpoints with Express.js

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const http = require('http')
 const express=require('express')
 
 const app=express()
-
+app.use(express.static("public"))
 
 // ------------------------Server Created By Using Express.js ------------------------------
 
 app.get("/",(req,res)=>{
     res.sendFile("index.html",{root:"."})
+})
+
+app.get("/menu",(req,res)=>{
+    res.sendFile("/views/menu.html",{root:"."})
+})
+
+app.get("/contact",(req,res)=>{
+    res.sendFile("/views/contact.html",{root:"."})
 })
 
 http.createServer(app).listen(3000)
